cea8aebd1b22a137b466d2da56e4550c17354667
Jericho Siahaya <jericho.siahaya@gmail.com>
2022-03-04 13:01:12 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
     <button @click="login" type="button" v-if="!user">Login with Microsoft</button>
     <button @click="callAPI" type="button" v-if="user">
       Call Graph's /me API
@@ -25,7 +23,6 @@ export default {
   name: 'app',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js + MSAL.js App',
       user: null,
       userInfo: null,
       apiCallFailed: false,
